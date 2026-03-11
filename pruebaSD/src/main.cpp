1e69fa1a0d8238f193c5f3a1ddbeffe8f234e0e6
Francesc <claderamirfrancesc@gmail.com>
2026-03-11 18:59:00 +0100

    sd funciona, falta bus spi custom

--- pruebaSD/src/main.cpp
+++ pruebaSD/src/main.cpp
@@ -3,18 +3,24 @@
 #include <SPI.h>
 #include <SD.h>
 
-#define SD_CS 10   // pin CS típico en Arduino
+#define SD_CS 5
 
 void setup() {
   Serial.begin(9600);
 
   if (!SD.begin(SD_CS)) {
     Serial.println("Error: no se pudo montar la SD");
-    while (1);
+    //while (1);
   }
 
-  Serial.println("SD montada correctamente");
+  //Serial.println("SD montada correctamente");
 }
 
 void loop() {
+  if (!SD.begin(SD_CS)) {
+    Serial.println("Error: no se pudo montar la SD");
+    //while (1);
+  }
+
+  delay(3000);
 }
